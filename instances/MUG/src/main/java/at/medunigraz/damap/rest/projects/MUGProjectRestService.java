65b5ff217993cc044ba64a65508c9fd7acebdda1
Laura <lthaci@tugraz.at>
2026-07-15 10:36:17 +0200

    chore(MUG): drop v4 project integration, Pure replaces MedUni project API

--- instances/MUG/src/main/java/at/medunigraz/damap/rest/projects/MUGProjectRestService.java
+++
@@ -1,7 +0,0 @@
-package at.medunigraz.damap.rest.projects;
-
-import at.medunigraz.api.rest.base.services.MUGAPIProjectServiceBase;
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
-@RegisterRestClient(configKey = "rest.projects")
-public interface MUGProjectRestService extends MUGAPIProjectServiceBase {}
